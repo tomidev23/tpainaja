8efe7ccfba538b46384706866ce532f3348e46d0
tomidev23 <tomingselingga2512@gmail.com>
2025-12-10 23:08:09 +0700

    BE:UpdateV4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+use function fake; 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -23,11 +23,11 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
+       return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
