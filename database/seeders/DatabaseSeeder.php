fff80b7cd29b679760abc70824e6a0a6d3989d7b
tomidev23 <tomingselingga2512@gmail.com>
2025-12-11 00:22:45 +0700

    BE:UpdateV8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,12 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
-User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'testing',
-            'role'=> 'admin'
-        ]);
-    }
+{
+    User::create([
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('password'), // atau config('app.admin_password')?
+        'role' => 'admin',
+    ]);
+}
 }
